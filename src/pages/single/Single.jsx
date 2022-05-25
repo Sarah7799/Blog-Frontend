787d29547f51738b8e61f8697e788f8c9c201045
Sarah7799 <psarahruth@gmail.com>
2022-05-25 20:13:16 +0530

    first commit

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import "./single.css";
+import SinglePost from "../../components/singlePost/SinglePost";
+import React from "react";
+
+
+export default function Single() {
+  return (
+    <div className="single">
+      <SinglePost/>
+    </div>
+  );
+}
