2a8bda0076724bbe698099c4f4334bc7dc59f5f0
Sarah Ruth <82445581+Sarah7799@users.noreply.github.com>
2022-05-30 20:19:14 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("https://explorer-thiru.herokuapp.com/api/posts" + search);
+      const res = await axios.get("/api/posts" + search);
       setPosts(res.data);
     };
     fetchPosts();
